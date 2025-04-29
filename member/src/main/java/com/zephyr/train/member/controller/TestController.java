659fcf996ad04dba2910dd1b3734ada629503bb6
Zhufeng-Qiu <zhufqiu@gmail.com>
2025-04-29 16:24:06 -0700

    4.4 member module: create member module

--- member/src/main/java/com/zephyr/train/member/controller/TestController.java
+++ member/src/main/java/com/zephyr/train/member/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.zephyr.train;
+package com.zephyr.train.member.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
   @GetMapping("/hello")
-  public String hello() {return "Hello World, Zhufeng";}
+  public String hello() {return "Hello World, member";}
 }
