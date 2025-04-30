1278bd5b097b43b89e42fe53647b44214c13fe16
Zhufeng-Qiu <zhufqiu@gmail.com>
2025-04-30 04:45:26 -0700

    4.15 member module: implement a simple register API

--- member/src/main/java/com/zephyr/train/member/service/MemberService.java
+++ member/src/main/java/com/zephyr/train/member/service/MemberService.java
@@ -1,7 +1,11 @@
 package com.zephyr.train.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.zephyr.train.member.domain.Member;
+import com.zephyr.train.member.domain.MemberExample;
 import com.zephyr.train.member.mapper.MemberMapper;
 import jakarta.annotation.Resource;
+import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,5 +16,21 @@ public class MemberService {
 
   public int count() {
     return Math.toIntExact(memberMapper.countByExample(null));
+  }
+
+  public long register(String mobile) {
+    MemberExample memberExample = new MemberExample();
+    memberExample.createCriteria().andMobileEqualTo(mobile);
+    List<Member> list = memberMapper.selectByExample(memberExample);
+
+    if (CollUtil.isNotEmpty(list)) {
+//      return list.get(0).getId();
+      throw new RuntimeException("mobile has been registered");
+    }
+    Member member = new Member();
+    member.setId(System.currentTimeMillis());
+    member.setMobile(mobile);
+    memberMapper.insert(member);
+    return member.getId();
   }
 }
