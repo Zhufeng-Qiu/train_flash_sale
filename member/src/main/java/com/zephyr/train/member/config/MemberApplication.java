0936fe56fb1964416485481448b7951be15bf863
Zhufeng-Qiu <zhufqiu@gmail.com>
2025-04-29 18:52:34 -0700

    4.5 member module: update startup log, add /member routing, modify logback log

--- member/src/main/java/com/zephyr/train/member/config/MemberApplication.java
+++ member/src/main/java/com/zephyr/train/member/config/MemberApplication.java
@@ -1,15 +1,22 @@
 package com.zephyr.train.member.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.zephyr")
 public class MemberApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(MemberApplication.class, args);
-  }
+  private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
 
+  public static void main(String[] args) {
+    SpringApplication app = new SpringApplication(MemberApplication.class);
+    Environment env = app.run(args).getEnvironment();
+    LOG.info("Launch successfully!");
+    LOG.info("Test Address: \thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
+  }
 }
