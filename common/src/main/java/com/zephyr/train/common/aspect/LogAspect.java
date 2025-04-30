a0e7ce41029875f11870cd83414270d09708a072
Zhufeng-Qiu <zhufqiu@gmail.com>
2025-04-29 19:41:34 -0700

    4.8 common module: create common module to place common classes, such as utility classes, interceptors, AOP aspects, constants, enums, and common configs.

--- common/src/main/java/com/zephyr/train/common/aspect/LogAspect.java
+++ common/src/main/java/com/zephyr/train/common/aspect/LogAspect.java
@@ -1,4 +1,4 @@
-package com.zephyr.train.member.aspect;
+package com.zephyr.train.common.aspect;
 
 import cn.hutool.core.util.RandomUtil;
 import com.alibaba.fastjson.JSONObject;
@@ -25,7 +25,7 @@ import org.springframework.web.multipart.MultipartFile;
 @Component
 public class LogAspect {
   public LogAspect() {
-    System.out.println("LogAspect");
+    System.out.println("Common LogAspect");
   }
 
   private final static Logger LOG = LoggerFactory.getLogger(LogAspect.class);
