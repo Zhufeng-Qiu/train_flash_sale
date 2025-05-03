0cf5a31c3f448513d9d66fca4920b66ba81ee7cd
Zhufeng-Qiu <zhufqiu@gmail.com>
2025-05-03 16:52:56 -0700

    7.8 member module: add passenger query-list API to view only the passengers that current member has added

--- member/src/main/java/com/zephyr/train/member/controller/PassengerController.java
+++ member/src/main/java/com/zephyr/train/member/controller/PassengerController.java
@@ -1,10 +1,15 @@
 package com.zephyr.train.member.controller;
 
+import com.zephyr.train.common.context.LoginMemberContext;
 import com.zephyr.train.common.resp.CommonResp;
+import com.zephyr.train.member.req.PassengerQueryReq;
 import com.zephyr.train.member.req.PassengerSaveReq;
+import com.zephyr.train.member.resp.PassengerQueryResp;
 import com.zephyr.train.member.service.PassengerService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +26,12 @@ public class PassengerController {
   public CommonResp<Object> save(@Valid @RequestBody PassengerSaveReq req) {
     passengerService.save(req);
     return new CommonResp<>();
+  }
+
+  @GetMapping("/query-list")
+  public CommonResp<Object> queryList(@Valid PassengerQueryReq req) {
+    req.setMemberId(LoginMemberContext.getId());
+    List<PassengerQueryResp> list = passengerService.queryList(req);
+    return new CommonResp<>(list);
   }
 }
--- member/src/main/java/com/zephyr/train/member/service/PassengerService.java
+++ member/src/main/java/com/zephyr/train/member/service/PassengerService.java
@@ -2,12 +2,17 @@ package com.zephyr.train.member.service;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjectUtil;
 import com.zephyr.train.common.context.LoginMemberContext;
 import com.zephyr.train.common.util.SnowUtil;
 import com.zephyr.train.member.domain.Passenger;
+import com.zephyr.train.member.domain.PassengerExample;
 import com.zephyr.train.member.mapper.PassengerMapper;
+import com.zephyr.train.member.req.PassengerQueryReq;
 import com.zephyr.train.member.req.PassengerSaveReq;
+import com.zephyr.train.member.resp.PassengerQueryResp;
 import jakarta.annotation.Resource;
+import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,10 +23,20 @@ public class PassengerService {
   public void save(PassengerSaveReq req) {
     DateTime now = DateTime.now();
     Passenger passenger = BeanUtil.copyProperties(req, Passenger.class);
-    passenger.setMemberId(LoginMemberContext.getId());
     passenger.setId(SnowUtil.getSnowflakeNextId());
+    passenger.setMemberId(LoginMemberContext.getId());
     passenger.setCreateTime(now);
     passenger.setUpdateTime(now);
     passengerMapper.insert(passenger);
+  }
+
+  public List<PassengerQueryResp> queryList(PassengerQueryReq req) {
+    PassengerExample passengerExample = new PassengerExample();
+    PassengerExample.Criteria criteria = passengerExample.createCriteria();
+    if (ObjectUtil.isNotNull(req.getMemberId())) {
+      criteria.andMemberIdEqualTo(req.getMemberId());
+    }
+    List<Passenger> passengerList = passengerMapper.selectByExample(passengerExample);
+    return BeanUtil.copyToList(passengerList, PassengerQueryResp.class);
   }
 }
