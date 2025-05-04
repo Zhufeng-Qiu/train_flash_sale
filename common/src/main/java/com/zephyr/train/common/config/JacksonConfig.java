a1798c3aec2ded44c81bc6c3489dca1a5f184706
Zhufeng-Qiu <zhufqiu@gmail.com>
2025-05-03 18:34:25 -0700

    7.11 member module: Fix the issue: precision loss for Long type during front-end and back-end interaction

---
+++ common/src/main/java/com/zephyr/train/common/config/JacksonConfig.java
@@ -0,0 +1,23 @@
+// package com.zephyr.train.common.config;
+//
+// import com.fasterxml.jackson.databind.ObjectMapper;
+// import com.fasterxml.jackson.databind.module.SimpleModule;
+// import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+//
+// /**
+//  * Fix the issue: precision loss for Long type during front-end and back-end interaction
+//  */
+// @Configuration
+// public class JacksonConfig {
+//     @Bean
+//     public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+//         ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+//         SimpleModule simpleModule = new SimpleModule();
+//         simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+//         objectMapper.registerModule(simpleModule);
+//         return objectMapper;
+//     }
+// }
