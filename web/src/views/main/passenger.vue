4f8078a20200974e7c5830334db57399f2e4a718
Zhufeng-Qiu <zhufqiu@gmail.com>
2025-05-03 19:40:28 -0700

    7.13 web module: implement passenger Edit interface

--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -2,14 +2,22 @@
   <p>
     <a-space>
       <a-button type="primary" @click="handleQuery()">Refresh</a-button>
-      <a-button type="primary" @click="showModal">Add</a-button>
+      <a-button type="primary" @click="onAdd">Add</a-button>
     </a-space>
   </p>
   <a-table :dataSource="passengers"
            :columns="columns"
            :pagination="pagination"
            @change="handleTableChange"
-           :loading="loading"/>
+           :loading="loading">
+    <template #bodyCell="{ column, record }">
+      <template v-if="column.dataIndex === 'operation'">
+        <a-space>
+          <a @click="onEdit(record)">Edit</a>
+        </a-space>
+      </template>
+    </template>
+  </a-table>
   <a-modal v-model:visible="visible" title="Passenger Info" @ok="handleOk"
            ok-text="Confirm" cancel-text="Cancel">
     <a-form :model="passenger" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
@@ -39,7 +47,7 @@ export default defineComponent({
   setup() {
     const visible = ref(false);
 
-    const passenger = reactive({
+    let passenger = ref({
       id: undefined,
       memberId: undefined,
       name: undefined,
@@ -51,7 +59,7 @@ export default defineComponent({
 
     const passengers = ref([]);
     // The name of three fields of pagination is fixed
-    const pagination = reactive({
+    const pagination = ref({
       total: 0,
       current: 1,
       pageSize: 2,
@@ -69,22 +77,31 @@ export default defineComponent({
       title: 'Type',
       dataIndex: 'type',
       key: 'type',
+    }, {
+      title: 'Operation',
+      dataIndex: 'operation'
     }];
 
-    const showModal = () => {
+    const onAdd = () => {
+      passenger.value = {};
+      visible.value = true;
+    };
+
+    const onEdit = (record) => {
+      passenger.value = window.Tool.copy(record);
       visible.value = true;
     };
 
     const handleOk = () => {
-      axios.post("/member/passenger/save", passenger).then((response) => {
+      axios.post("/member/passenger/save", passenger.value).then((response) => {
         let data = response.data;
         if (data.success) {
           notification.success({description: "Save successfully！"});
           visible.value = false;
           handleQuery({
-            // page: pagination.current, // stay at current page after adding
-            page: 1, // return to first page after adding
-            size: pagination.pageSize
+            page: pagination.value.current, // stay at current page after adding
+            // page: 1, // return to first page after adding
+            size: pagination.value.pageSize
           });
         } else {
           notification.error({description: data.message});
@@ -96,7 +113,7 @@ export default defineComponent({
       if (!param) {
         param = {
           page: 1,
-          size: pagination.pageSize
+          size: pagination.value.pageSize
         };
       }
       loading.value = true;
@@ -110,8 +127,8 @@ export default defineComponent({
         let data = response.data;
         if (data.success) {
           passengers.value = data.content.list;
-          pagination.current = param.page;
-          pagination.total = data.content.total;
+          pagination.value.current = param.page;
+          pagination.value.total = data.content.total;
         } else {
           notification.error({description: data.message});
         }
@@ -128,7 +145,7 @@ export default defineComponent({
     onMounted(() => {
       handleQuery({
         page: 1,
-        size: pagination.pageSize
+        size: pagination.value.pageSize
       });
     });
 
@@ -139,7 +156,8 @@ export default defineComponent({
       pagination,
       columns,
       loading,
-      showModal,
+      onAdd,
+      onEdit,
       handleOk,
       handleTableChange,
       handleQuery
