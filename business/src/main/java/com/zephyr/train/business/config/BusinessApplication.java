8c9eaea5e507f26515329b5ca4b68d84efbf954c
Zhufeng-Qiu <zhufqiu@gmail.com>
2025-05-06 18:25:08 -0700

    9.4 business module: add business module

--- business/src/main/java/com/zephyr/train/business/config/BusinessApplication.java
+++ business/src/main/java/com/zephyr/train/business/config/BusinessApplication.java
@@ -1,4 +1,5 @@
-package com.zephyr.train.member.config;
+package com.zephyr.train.business.config;
+
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
@@ -11,12 +12,12 @@ import org.springframework.core.env.Environment;
 @SpringBootApplication
 @ComponentScan("com.zephyr")
 @MapperScan("com.zephyr.train.*.mapper")
-public class MemberApplication {
+public class BusinessApplication {
 
-  private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
+  private static final Logger LOG = LoggerFactory.getLogger(BusinessApplication.class);
 
   public static void main(String[] args) {
-    SpringApplication app = new SpringApplication(MemberApplication.class);
+    SpringApplication app = new SpringApplication(BusinessApplication.class);
     Environment env = app.run(args).getEnvironment();
     LOG.info("Launch successfully!");
     LOG.info("Test Address: \thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
