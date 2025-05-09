89c7f0e60bb5fe9185f4ce171600e1031ae1a933
Zhufeng-Qiu <zhufqiu@gmail.com>
2025-05-08 19:09:40 -0700

    10.2 batch module: add batch scheduled task module

---
+++ batch/src/main/java/com/zephyr/train/batch/config/BatchApplication.java
@@ -0,0 +1,25 @@
+package com.zephyr.train.batch.config;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@ComponentScan("com.zephyr")
+@MapperScan("com.zephyr.train.*.mapper")
+public class BatchApplication {
+
+  private static final Logger LOG = LoggerFactory.getLogger(BatchApplication.class);
+
+  public static void main(String[] args) {
+    SpringApplication app = new SpringApplication(BatchApplication.class);
+    Environment env = app.run(args).getEnvironment();
+    LOG.info("Launch successfully!");
+    LOG.info("Test Address: \thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
+  }
+}
