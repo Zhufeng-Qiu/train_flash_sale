1c916222664281b4eb55e9e367f34279ebab267b
Zhufeng-Qiu <zhufqiu@gmail.com>
2025-05-14 23:11:49 -0700

    12.5 admin/business module: daily remaining ticket query feature

--- business/src/main/java/com/zephyr/train/business/req/DailyTrainTicketQueryReq.java
+++ business/src/main/java/com/zephyr/train/business/req/DailyTrainTicketQueryReq.java
@@ -1,12 +1,71 @@
 package com.zephyr.train.business.req;
 
 import com.zephyr.train.common.req.PageReq;
+import java.util.Date;
+import org.springframework.format.annotation.DateTimeFormat;
 
 public class DailyTrainTicketQueryReq extends PageReq {
+
+    /**
+     * Date
+     */
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+
+    /**
+     * Train number
+     */
+    private String trainCode;
+
+    /**
+     * Departure station
+     */
+    private String start;
+
+    /**
+     * Arrival station
+     */
+    private String end;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public void setTrainCode(String trainCode) {
+        this.trainCode = trainCode;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
 
     @Override
     public String toString() {
         return "DailyTrainTicketQueryReq{" +
-                "} " + super.toString();
+            "date=" + date +
+            ", trainCode='" + trainCode + '\'' +
+            ", start='" + start + '\'' +
+            ", end='" + end + '\'' +
+            "} " + super.toString();
     }
 }
