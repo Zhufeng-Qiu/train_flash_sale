457a834b03f0ba7bca2256af70fd78052db15a8c
Zhufeng-Qiu <zhufqiu@gmail.com>
2025-05-15 21:05:08 -0700

    12.7 web module: display seat info in order page

--- web/src/views/main/order.vue
+++ web/src/views/main/order.vue
@@ -8,6 +8,14 @@
     <span class="order-train-main">——</span>&nbsp;
     <span class="order-train-main">Arrival: {{dailyTrainTicket.endPinyin}}</span>
     <span class="order-train-main">({{dailyTrainTicket.endTime}})</span>&nbsp;
+
+    <div class="order-train-ticket">
+      <span v-for="item in seatTypes" :key="item.type">
+        <span>{{item.desc}}</span>：
+        <span class="order-train-ticket-main">${{item.price}}</span>&nbsp;
+        <span class="order-train-ticket-main">{{item.count}}</span>&nbsp;seats left&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      </span>
+    </div>
   </div>
 </template>
 
@@ -21,8 +29,35 @@ export default defineComponent({
     const dailyTrainTicket = SessionStorage.get(SESSION_ORDER) || {};
     console.log("Order info for selected order", dailyTrainTicket);
 
+    const SEAT_TYPE = window.SEAT_TYPE;
+    console.log(SEAT_TYPE)
+    // Provided seat types of current train number, including price, remaining tickets, etc. For example:
+    // {
+    //   type: "YDZ",
+    //   code: "1",
+    //   desc: "First Class Seat",
+    //   count: "100",
+    //   price: "50",
+    // }
+    // For SEAT_TYPE[KEY]: when knowing a specific property name(xxx), use obj.xxx; when the property name is a variable, use obj[xxx].
+    const seatTypes = [];
+    for (let KEY in SEAT_TYPE) {
+      let key = KEY.toLowerCase();
+      if (dailyTrainTicket[key] >= 0) {
+        seatTypes.push({
+          type: KEY,
+          code: SEAT_TYPE[KEY]["code"],
+          desc: SEAT_TYPE[KEY]["desc"],
+          count: dailyTrainTicket[key],
+          price: dailyTrainTicket[key + 'Price'],
+        })
+      }
+    }
+    console.log("Provided seat types: ", seatTypes)
+
     return {
-      dailyTrainTicket
+      dailyTrainTicket,
+      seatTypes
     };
   },
 });
@@ -32,5 +67,12 @@ export default defineComponent({
 .order-train .order-train-main {
   font-size: 18px;
   font-weight: bold;
+}
+.order-train .order-train-ticket {
+  margin-top: 15px;
+}
+.order-train .order-train-ticket .order-train-ticket-main {
+  color: green;
+  font-size: 18px;
 }
 </style>
