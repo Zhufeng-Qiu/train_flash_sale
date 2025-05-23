89221a06447dbdc46c0d47a972bd5fc24fbeb56d
Zhufeng-Qiu <zhufqiu@gmail.com>
2025-05-23 16:55:00 -0700

    13.9 gateway module: config feign+nacos to remote invoke based on application name

--- batch/src/main/java/com/zephyr/train/batch/feign/BusinessFeign.java
+++ batch/src/main/java/com/zephyr/train/batch/feign/BusinessFeign.java
@@ -7,12 +7,13 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "business", url = "http://127.0.0.1:8002/business")
+//@FeignClient(name = "business", url = "http://127.0.0.1:8002/business")
+@FeignClient("business")
 public interface BusinessFeign {
 
-  @GetMapping("/hello")
+  @GetMapping("/business/hello")
   String hello();
 
-  @GetMapping("/admin/daily-train/gen-daily/{date}")
+  @GetMapping("/business/admin/daily-train/gen-daily/{date}")
   CommonResp<Object> genDaily(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") Date date);
 }
