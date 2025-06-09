3762c9c468fb2f6f33fa6fbe1a1006feaf8e16c1
Zhufeng-Qiu <zhufqiu@gmail.com>
2025-06-09 07:20:05 -0700

    19.11 business module: update query-line-count UI

--- web/src/views/main/order.vue
+++ web/src/views/main/order.vue
@@ -135,10 +135,16 @@
     <a-button type="danger" block @click="validFirstImageCode">Submit CAPTCHA</a-button>
   </a-modal>
 
-  <a-modal v-model:visible="lineModalVisible" :title="null" :footer="null" :maskClosable="false" :closable="false"
+  <a-modal v-model:visible="lineModalVisible" title="Queue up for tickets" :footer="null" :maskClosable="false" :closable="false"
            style="top: 50px; width: 400px">
     <div class="book-line">
-      <loading-outlined /> Confirm order: {{confirmOrderId}}, System Processing...
+      <div v-show="confirmOrderLineCount < 0">
+        <loading-outlined /> System Processing...
+      </div>
+      <div v-show="confirmOrderLineCount >= 0">
+        <loading-outlined /> There are {{confirmOrderLineCount}} users ahead of you purchasing tickets. You’re in the queue. Please wait a moment.
+
+      </div>
     </div>
   </a-modal>
 </template>
@@ -199,6 +205,7 @@ export default defineComponent({
     const visible = ref(false);
     const lineModalVisible = ref(false);
     const confirmOrderId = ref();
+    const confirmOrderLineCount = ref(-1);
 
     // Check or uncheck a passenger then add or remove a record
     watch(() => passengerChecks.value, (newVal, oldVal)=>{
@@ -377,11 +384,50 @@ export default defineComponent({
           imageCodeModalVisible.value = false;
           lineModalVisible.value = true;
           confirmOrderId.value = data.content;
+          queryLineCount();
         } else {
           notification.error({description: data.message});
         }
       });
     }
+
+    /* ------------------- 定时查询订单状态 --------------------- */
+    // 确认订单后定时查询
+    let queryLineCountInterval;
+
+    // 定时查询订单结果/排队数量
+    const queryLineCount = () => {
+      confirmOrderLineCount.value = -1;
+      queryLineCountInterval = setInterval(function () {
+        axios.get("/business/confirm-order/query-line-count/" + confirmOrderId.value).then((response) => {
+          let data = response.data;
+          if (data.success) {
+            let result = data.content;
+            switch (result) {
+              case -1 :
+                notification.success({description: "Ticket purchase successful!"});
+                lineModalVisible.value = false;
+                clearInterval(queryLineCountInterval);
+                break;
+              case -2:
+                notification.error({description: "Ticket purchase failed."});
+                lineModalVisible.value = false;
+                clearInterval(queryLineCountInterval);
+                break;
+              case -3:
+                notification.error({description: "Sorry, no ticket available."});
+                lineModalVisible.value = false;
+                clearInterval(queryLineCountInterval);
+                break;
+              default:
+                confirmOrderLineCount.value = result;
+            }
+          } else {
+            notification.error({description: data.message});
+          }
+        });
+      }, 500);
+    };
 
     /* ------------------- Second CAPTCHA --------------------- */
     const imageCodeModalVisible = ref();
@@ -469,7 +515,8 @@ export default defineComponent({
       showFirstImageCodeModal,
       validFirstImageCode,
       lineModalVisible,
-      confirmOrderId
+      confirmOrderId,
+      confirmOrderLineCount
     };
   },
 });
